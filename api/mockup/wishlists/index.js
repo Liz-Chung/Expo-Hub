5f68f0949fecb7779777147d1e9a651f35fba1ce
Liz Chung <lizzhongx@gmail.com>
2024-07-05 17:04:01 +0900

    no message

--- api/mockup/wishlists/index.js
+++
@@ -1,8 +0,0 @@
-import { wishlists } from '../../data';
-import cors, { runMiddleware } from '../../corsMiddleware';
-
-export default async function handler(req, res) {
-  await runMiddleware(req, res, cors);
-
-  res.status(200).json(wishlists);
-}
